db5568fae17fda631d0032d022c48c30bca56702
Cauã Henrique <147276764+cauasenna@users.noreply.github.com>
2025-03-29 16:24:46 -0300

    Delete at-11.py

--- at-11.py
+++
@@ -1,9 +0,0 @@
-# Request username and password from the user
-username = input("Enter your username: ")
-password = input("Enter your password: ")
-
-# Check if the username and password are the same
-if username == password:
-    print("Error: Username and password cannot be the same.")
-else:
-    print("Login successful!")
