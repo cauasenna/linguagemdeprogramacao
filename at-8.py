4ce24d4b690808db45e1650e4384359792425b04
Cauã Henrique <147276764+cauasenna@users.noreply.github.com>
2025-03-29 16:26:01 -0300

    Delete at-8.py

--- at-8.py
+++
@@ -1,8 +0,0 @@
-# Request the temperature in Celsius
-celsius = float(input("Enter the temperature in Celsius: "))
-
-# Convert Celsius to Fahrenheit
-fahrenheit = (celsius * 9/5) + 32
-
-# Display the result
-print(f"The temperature in Fahrenheit is: {fahrenheit}")
